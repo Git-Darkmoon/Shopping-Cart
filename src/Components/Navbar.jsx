8142d8d5611b4b5b0d414f0b017cdfa266238129
Nicolas Mayorga <namayorgav@gmail.com>
2023-07-01 12:03:44 -0500

    added: Navbar Component

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { FaShoppingCart } from "react-icons/fa"
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">iCart</div>
+      <div className="cartIndicator">
+        <FaShoppingCart id="cartIcon" />
+        <span id="cartAmount">2</span>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
