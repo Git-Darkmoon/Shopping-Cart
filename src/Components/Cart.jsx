5fe0f542ba1215d48dd03c8776acd7aa5eb80e3c
Nicolas Mayorga <namayorgav@gmail.com>
2023-07-03 12:27:59 -0500

    feat: Empty Cart btn and return.

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -2,18 +2,26 @@ import Product from "./Product"
 import { useGlobalContext } from "./context"
 
 function Cart() {
-  const { cartInfo } = useGlobalContext()
+  const { cartInfo, clearCart } = useGlobalContext()
 
   const cartArray = Array.from(cartInfo.entries())
+
+  if (cartArray.length === 0) {
+    return <h1 className="emptyTitle shine">Empty Cart</h1>
+  }
 
   return (
     <main className="cartContainer">
       <h1 className="cartTitle">Your Cart</h1>
+
       {cartArray.map((product) => {
         const [id, productInfo] = product
 
         return <Product key={id} {...productInfo} />
       })}
+      <button className="clearBtn" onClick={clearCart}>
+        Clear Cart
+      </button>
     </main>
   )
 }
