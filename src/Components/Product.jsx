b2d47a427db377ec7da75034edaffde09160023a
Nicolas Mayorga <namayorgav@gmail.com>
2023-07-03 12:28:48 -0500

    feat: onClick events.

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,7 +1,10 @@
 /* eslint-disable react/prop-types */
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai"
+import { useGlobalContext } from "./context"
 
 function Product({ id, title, price, img, amount }) {
+  const { increaseQuantity, decreaseQuantity, removeItem } = useGlobalContext()
+
   return (
     <article className="product">
       <picture>
@@ -12,15 +15,25 @@ function Product({ id, title, price, img, amount }) {
           <h4 className="productRef">Ref: {id.slice(0, 17)} </h4>
           <h1 className="productName">{title}</h1>
           <h2 className="productPrice">$ {price}</h2>
-          <button className="productRemove">remove</button>
+          <button className="productRemove" onClick={() => removeItem(id)}>
+            remove
+          </button>
         </div>
 
         <div className="quantity">
-          <button className="quantityBtn" id="decreaseBtn">
+          <button
+            className="quantityBtn"
+            id="decreaseBtn"
+            onClick={() => decreaseQuantity(id)}
+          >
             <AiOutlineMinus />
           </button>
           <span id="productQuantity">{amount}</span>
-          <button className="quantityBtn" id="increaseBtn">
+          <button
+            className="quantityBtn"
+            id="increaseBtn"
+            onClick={() => increaseQuantity(id)}
+          >
             <AiOutlinePlus />
           </button>
         </div>
