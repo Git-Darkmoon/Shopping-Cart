af81ff622bc4a034f293b1bd14deedd1a6159ece
Nicolas Mayorga <namayorgav@gmail.com>
2023-07-03 12:27:12 -0500

    feat: document title change onBlur.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { useEffect } from "react"
 import Cart from "./Components/Cart"
+import Footer from "./Components/Footer"
 import Navbar from "./Components/Navbar"
 
 function App() {
+  useEffect(() => {
+    window.addEventListener("blur", () => {
+      document.title = "2 products are waiting"
+    })
+    window.addEventListener("focus", () => {
+      document.title = "Shopping Cart"
+    })
+  })
+
   return (
     <>
       <Navbar />
       <Cart />
+      <Footer />
     </>
   )
 }
