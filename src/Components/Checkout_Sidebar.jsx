b89bf3ffa8168b705a9b8885eb346f10198ba560
Nicolas Mayorga <namayorgav@gmail.com>
2023-07-04 15:37:09 -0500

    feat: closeSidebar feature.

--- src/Components/Checkout_Sidebar.jsx
+++ src/Components/Checkout_Sidebar.jsx
@@ -1,38 +1,47 @@
 import { FaTimes } from "react-icons/fa"
+import { useGlobalContext } from "./context"
 
 function Checkout_Sidebar() {
+  const { isSideOpen, closeSidebar } = useGlobalContext()
+
   return (
-    <aside className="sidebar">
-      <div className="sidebar-card">
-        <header className="sidebar-header">
-          <h1 className="logo">Summary</h1>
-          <button className="sidebar-close_btn">
-            <FaTimes />
-          </button>
-        </header>
-        <div className="summary-flex subtotal">
-          <div className="subtotal-text">
-            <h2>Subtotal - 2 items</h2>
+    <div className={`sidebar-overlay ${isSideOpen && "show-sideBar"}`}>
+      <aside className="sidebar">
+        <form className="sidebar-card">
+          <header className="sidebar-header">
+            <h1 className="sidebar-title">Summary</h1>
+            <button
+              type="button"
+              className="sidebar-close_btn"
+              onClick={closeSidebar}
+            >
+              <FaTimes />
+            </button>
+          </header>
+          <div className="summary-flex subtotal">
+            <div className="subtotal-text">
+              <h2>Subtotal - 2 items</h2>
+            </div>
+            <div className="subtotal-price summary-price">
+              <h3>$600</h3>
+            </div>
           </div>
-          <div className="subtotal-price">
-            <h3>$ 635</h3>
+          <div className="shipping">
+            <h3>Shipping Price</h3>
+            <h2>$20</h2>
           </div>
-        </div>
-        <div className="promo-code">
-          <input type="text" name="promo" id="promo" />
-          <button className="promoBtn">Apply</button>
-        </div>
-        <div className="summary-flex total">
-          <div className="total-text">
-            <h2>Order Total</h2>
+          <div className="summary-flex total">
+            <div className="total-text">
+              <h2>Order Total</h2>
+            </div>
+            <div className="total-price summary-price">
+              <h3>$620</h3>
+            </div>
           </div>
-          <div className="total-price">
-            <h3>$ 635</h3>
-          </div>
-        </div>
-        <button className="checkoutBtn">Checkout</button>
-      </div>
-    </aside>
+          <button className="checkoutBtn">Checkout</button>
+        </form>
+      </aside>
+    </div>
   )
 }
 
