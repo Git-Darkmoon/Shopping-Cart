8d65829b03ee5f47158e37a13752f41bbecfa85e
Nicolas Mayorga <namayorgav@gmail.com>
2023-07-04 15:37:18 -0500

    feat: openSidebar.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,14 @@
 import { FaShoppingCart } from "react-icons/fa"
+import { useGlobalContext } from "./context"
 
 function Navbar() {
+  const { openSidebar } = useGlobalContext()
+
   return (
     <nav className="navbar">
       <div className="logo">iTech</div>
       <div className="cartIndicator">
-        <FaShoppingCart id="cartIcon" />
+        <FaShoppingCart id="cartIcon" onClick={openSidebar} />
         <span id="cartAmount">2</span>
       </div>
     </nav>
