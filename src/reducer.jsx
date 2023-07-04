f636b4369460490e7f58511082e3d6307a3f0a00
Nicolas Mayorga <namayorgav@gmail.com>
2023-07-04 12:20:12 -0500

    .

--- src/reducer.jsx
+++ src/reducer.jsx
@@ -32,7 +32,7 @@ function reducer(state, action) {
     const productID = action.payload.id
     const product = newCart.get(productID)
 
-    const newProduct = { ...product, amount: product.amount++ }
+    const newProduct = { ...product, amount: (product.amount += 1) }
     newCart.set(productID, newProduct)
 
     console.log(newProduct)
